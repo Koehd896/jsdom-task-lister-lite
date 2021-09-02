f7ec955d1af1343e2625ffac9accc6235e0a2832
Raven <ravenkoehler@gmail.com>
2021-09-02 16:11:51 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form");
+  form.addEventListener("submit", (event) => {
+    let taskDescription = document.getElementById("new-task-description").value;
+    let taskList = document.getElementById("tasks");
+    let newListItem = document.createElement("li");
+    newListItem.innerHTML = taskDescription;
+    taskList.appendChild(newListItem);
+    event.preventDefault();
+  })
 });
